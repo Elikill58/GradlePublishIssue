79f298fb014c37e7ae894f0692e6b8bb913d3e8e
Elikill58 <arpetzouille@gmail.com>
2024-09-28 14:42:06 +0200

    Add the code

---
+++ src/com/elikill58/test/GradleIssue.java
@@ -0,0 +1,14 @@
+package com.elikill58.test;
+
+public class GradleIssue {
+
+    /**
+     * Check if the issue is here.<br>
+     * This method does nothing.
+     * 
+     * @param msg the message if the issue is here
+     */
+    public static void checkIfIssue(String msg) {
+        
+    }
+}
